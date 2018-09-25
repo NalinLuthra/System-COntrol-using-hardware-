afff195c03066dda96c3ffc43de4f563c15746d2
Nalin <nalin@Mr.Blue>
2018-09-25 09:58:18 +0530

    Data Transmission

---
+++ src/my/SystemControl/info.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.SystemControl;
+
+/**
+ *
+ * @author nalin
+ */
+public class info {
+    String name;
+    String age;
+    String ID;
+}
---
+++ src/system/control/NewClass.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package system.control;
+import gnu.io.*;
+import static system.control.NewClass.listPorts;
+    
+/**
+ *
+ * @author nalin
+ */
+public class NewClass {
+    static void listPorts()
+    {
+        java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
+        while ( portEnum.hasMoreElements() ) 
+        {
+            CommPortIdentifier portIdentifier = portEnum.nextElement();
+            System.out.println(portIdentifier.getName()  +  " - " +  getPortTypeName(portIdentifier.getPortType()) );
+        }        
+    }
+    
+    static String getPortTypeName ( int portType )
+    {
+        switch ( portType )
+        {
+            case CommPortIdentifier.PORT_I2C:
+                return "I2C";
+            case CommPortIdentifier.PORT_PARALLEL:
+                return "Parallel";
+            case CommPortIdentifier.PORT_RAW:
+                return "Raw";
+            case CommPortIdentifier.PORT_RS485:
+                return "RS485";
+            case CommPortIdentifier.PORT_SERIAL:
+                return "Serial";
+            default:
+                return "unknown type";
+        }
+    }
+    
+    public static void main(String args[]){
+        listPorts();
+    }
+}
